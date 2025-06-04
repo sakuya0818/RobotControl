f73699cab3891860d0ea4135a0aa1a250bae04d5
sakuya0818 <1015903494@qq.com>
2025-06-04 16:59:07 +0800

    新增系统信息的获取并发布消息

--- test/chapt2/topic_practice/install/setup.ps1
+++ test/chapt2/topic_practice/install/setup.ps1
@@ -23,7 +23,6 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/mnt/e/MyWorkSpace/C++/Ros/test/chapt2/demo_topic/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
