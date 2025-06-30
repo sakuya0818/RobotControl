6a9604d5c0860db06a6489873515c8cb8da98c72
sakuya0818 <1015903494@qq.com>
2025-06-30 21:39:46 +0800

    动态TF

--- test/chapt4/src/demo_tf/src/dynamic_tf_broadcaster.cpp
+++ test/chapt4/src/demo_tf/src/dynamic_tf_broadcaster.cpp
@@ -2,16 +2,17 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"  // 提供消息接口
 #include "tf2/LinearMath/Quaternion.h"              // 提供tf2::Quaternion类
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"  // 提供消息类型转换函数
-#include "tf2_ros/static_transform_broadcaster.h"   // 提供静态变换广播器
+#include "tf2_ros/transform_broadcaster.h"          // 提供变换广播器
+#include <chrono>
 
-class StaticTFBroadcaster : public rclcpp::Node
+class TFBroadcaster : public rclcpp::Node
 {
 public:
-  StaticTFBroadcaster() : Node("static_tf_broadcaster")
+  TFBroadcaster() : Node("tf_broadcaster")
   {
     // 创建静态变换广播器
-    static_tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
-    publish_static_tf();
+    static_tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+    timer_ = create_wall_timer(std::chrono::milliseconds(1000), std::bind(&TFBroadcaster::publish_static_tf, this));
   }
 
   void publish_static_tf()
@@ -20,27 +21,28 @@ public:
     // 设置变换消息的属性
     transform_stamped.header.stamp = this->get_clock()->now();
     transform_stamped.header.frame_id = "map";
-    transform_stamped.child_frame_id = "target_point";
+    transform_stamped.child_frame_id = "base_link";
 
     // 设置变换消息的坐标系
-    transform_stamped.transform.translation.x = 5.0;
+    transform_stamped.transform.translation.x = 2.0;
     transform_stamped.transform.translation.y = 3.0;
     transform_stamped.transform.translation.z = 0.0;
 
     tf2::Quaternion q;
-    q.setRPY(0.0, 0.0, 60 * M_PI / 180.0);
+    q.setRPY(0.0, 0.0, 30 * M_PI / 180.0);
     transform_stamped.transform.rotation = tf2::toMsg(q);
     this->static_tf_broadcaster_->sendTransform(transform_stamped);
   }
 
   private:
-    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster_;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> static_tf_broadcaster_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<StaticTFBroadcaster>());
+  rclcpp::spin(std::make_shared<TFBroadcaster>());
   rclcpp::shutdown();
   return 0;
 }
